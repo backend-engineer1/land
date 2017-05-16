bryanvu/lnd

lnwallet: add FeeEstimator interface, StaticFeeEstimator implementation
This commit adds the FeeEstimator interface, which can be used for
future fee calculation implementations. Currently, there is only the
StaticFeeEstimator implementation, which returns the same fee rate for
any transaction.

peer.go
@@ -284,7 +284,7 @@ func (p *peer) loadActiveChannels(chans []*channeldb.OpenChannel) error {
 		}
 
 		lnChan, err := lnwallet.NewLightningChannel(p.server.lnwallet.Signer,
-			p.server.chainNotifier, dbChan)
+			p.server.chainNotifier, p.server.lnwallet, dbChan)
 		if err != nil {
 			return err
 		}

backend-engineer1/land

peer.go
@@ -288,7 +288,7 @@ func (p *peer) loadActiveChannels(chans []*channeldb.OpenChannel) error {
 		}
 
 		lnChan, err := lnwallet.NewLightningChannel(p.server.lnwallet.Signer,
-			p.server.chainNotifier, dbChan)
+			p.server.chainNotifier, p.server.feeEstimator, dbChan)
 		if err != nil {
 			return err
 		}