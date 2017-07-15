AndrewSamokhvalov/lnd

htlcswitch+router: add onion error obfuscation
Within the network, it's important that when an HTLC forwarding failure
occurs, the recipient is notified in a timely manner in order to ensure
that errors are graceful and not unknown. For that reason with
accordance to BOLT №4 onion failure obfuscation have been added.

lnd_test.go
@@ -2026,10 +2026,11 @@ out:
 	} else if resp.PaymentError == "" {
 		t.Fatalf("payment should have been rejected due to invalid " +
 			"payment hash")
-	} else if !strings.Contains(resp.PaymentError, "preimage") {
+	} else if !strings.Contains(resp.PaymentError,
+		lnwire.CodeUnknownPaymentHash.String()) {
 		// TODO(roasbeef): make into proper gRPC error code
-		t.Fatalf("payment should have failed due to unknown preimage, "+
-			"instead failed due to : %v", err)
+		t.Fatalf("payment should have failed due to unknown payment hash, "+
+			"instead failed due to: %v", resp.PaymentError)
 	}
 
 	// The balances of all parties should be the same as initially since
@@ -2062,9 +2063,10 @@ out:
 	} else if resp.PaymentError == "" {
 		t.Fatalf("payment should have been rejected due to wrong " +
 			"HTLC amount")
-	} else if !strings.Contains(resp.PaymentError, "htlc value") {
+	} else if !strings.Contains(resp.PaymentError,
+		lnwire.CodeIncorrectPaymentAmount.String()) {
 		t.Fatalf("payment should have failed due to wrong amount, "+
-			"instead failed due to: %v", err)
+			"instead failed due to: %v", resp.PaymentError)
 	}
 
 	// The balances of all parties should be the same as initially since
@@ -2125,9 +2127,10 @@ out:
 	} else if resp.PaymentError == "" {
 		t.Fatalf("payment should fail due to insufficient "+
 			"capacity: %v", err)
-	} else if !strings.Contains(resp.PaymentError, "capacity") {
+	} else if !strings.Contains(resp.PaymentError,
+		lnwire.CodeInsufficientCapacity.String()) {
 		t.Fatalf("payment should fail due to insufficient capacity, "+
-			"instead: %v", err)
+			"instead: %v", resp.PaymentError)
 	}
 
 	// For our final test, we'll ensure that if a target link isn't
@@ -2154,9 +2157,10 @@ out:
 		t.Fatalf("payment stream has been closed: %v", err)
 	} else if resp.PaymentError == "" {
 		t.Fatalf("payment should have failed")
-	} else if !strings.Contains(resp.PaymentError, "hop unknown") {
+	} else if !strings.Contains(resp.PaymentError,
+		lnwire.CodeUnknownNextPeer.String()) {
 		t.Fatalf("payment should fail due to unknown hop, instead: %v",
-			err)
+			resp.PaymentError)
 	}
 
 	// Finally, immediately close the channel. This function will also
@@ -2571,17 +2575,18 @@ func testAsyncPayments(net *networkHarness, t *harnessTest) {
 
 		if err := alicePayStream.Send(sendReq); err != nil {
 			t.Fatalf("unable to send payment: "+
-				"%v", err)
+				"stream has been closed: %v", err)
 		}
 	}
 
+	// We should receive one insufficient capacity error, because we are
+	// sending on one invoice bigger.
 	errorReceived := false
 	for i := 0; i < numInvoices; i++ {
 		if resp, err := alicePayStream.Recv(); err != nil {
 			t.Fatalf("payment stream have been closed: %v", err)
 		} else if resp.PaymentError != "" {
-			if strings.Contains(resp.PaymentError,
-				"Insufficient") {
+			if strings.Contains(resp.PaymentError, "InsufficientCapacity") {
 				if errorReceived {
 					t.Fatalf("redundant payment "+
 						"error: %v", resp.PaymentError)
@@ -2591,7 +2596,7 @@ func testAsyncPayments(net *networkHarness, t *harnessTest) {
 				continue
 			}
 
-			t.Fatalf("unable to send payment: %v", err)
+			t.Fatalf("unable to send payment: %v", resp.PaymentError)
 		}
 	}
 

routing/router.go
@@ -17,8 +17,11 @@ import (
 	"github.com/roasbeef/btcd/wire"
 	"github.com/roasbeef/btcutil"
 
+	"crypto/sha256"
+
 	"github.com/go-errors/errors"
 	"github.com/lightningnetwork/lightning-onion"
+	"github.com/lightningnetwork/lnd/htlcswitch"
 )
 
 // ChannelGraphSource represent the source of information about the topology of
@@ -111,8 +114,8 @@ type Config struct {
 	// forward a fully encoded payment to the first hop in the route
 	// denoted by its public key. A non-nil error is to be returned if the
 	// payment was unsuccessful.
-	SendToSwitch func(firstHop *btcec.PublicKey,
-		htlcAdd *lnwire.UpdateAddHTLC) ([32]byte, error)
+	SendToSwitch func(firstHop *btcec.PublicKey, htlcAdd *lnwire.UpdateAddHTLC,
+		deobfuscator htlcswitch.Deobfuscator) ([sha256.Size]byte, error)
 }
 
 // routeTuple is an entry within the ChannelRouter's route cache. We cache
@@ -847,11 +850,12 @@ func (r *ChannelRouter) FindRoutes(target *btcec.PublicKey, amt btcutil.Amount) 
 	return validRoutes, nil
 }
 
-// generateSphinxPacket generates then encodes a sphinx packet which encodes
+// generateOnionBlob generates then encodes a sphinx packet which encodes
 // the onion route specified by the passed layer 3 route. The blob returned
 // from this function can immediately be included within an HTLC add packet to
 // be sent to the first hop within the route.
-func generateSphinxPacket(route *Route, paymentHash []byte) ([]byte, error) {
+func generateOnionBlob(route *Route, paymentHash []byte) ([]byte,
+	htlcswitch.Deobfuscator, error) {
 	// First obtain all the public keys along the route which are contained
 	// in each hop.
 	nodes := make([]*btcec.PublicKey, len(route.Hops))
@@ -877,7 +881,7 @@ func generateSphinxPacket(route *Route, paymentHash []byte) ([]byte, error) {
 
 	sessionKey, err := btcec.NewPrivateKey(btcec.S256())
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	// Next generate the onion routing packet which allows us to perform
@@ -885,14 +889,14 @@ func generateSphinxPacket(route *Route, paymentHash []byte) ([]byte, error) {
 	sphinxPacket, err := sphinx.NewOnionPacket(nodes, sessionKey,
 		hopPayloads, paymentHash)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	// Finally, encode Sphinx packet using it's wire representation to be
 	// included within the HTLC add packet.
 	var onionBlob bytes.Buffer
 	if err := sphinxPacket.Encode(&onionBlob); err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	log.Tracef("Generated sphinx packet: %v",
@@ -904,7 +908,16 @@ func generateSphinxPacket(route *Route, paymentHash []byte) ([]byte, error) {
 		}),
 	)
 
-	return onionBlob.Bytes(), nil
+	// Initialize the onion error deobfuscator in order to be able to decode
+	// the onion failure.
+	onionErrorDeobfuscator, err := sphinx.NewOnionDeobfuscator(sessionKey, nodes)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return onionBlob.Bytes(), &htlcswitch.FailureDeobfuscator{
+		OnionDeobfuscator: onionErrorDeobfuscator,
+	}, nil
 }
 
 // LightningPayment describes a payment to be sent through the network to the
@@ -989,7 +1002,8 @@ func (r *ChannelRouter) SendPayment(payment *LightningPayment) ([32]byte, *Route
 		// Generate the raw encoded sphinx packet to be included along
 		// with the htlcAdd message that we send directly to the
 		// switch.
-		sphinxPacket, err := generateSphinxPacket(route, payment.PaymentHash[:])
+		onionBlob, failureObfuscator, err := generateOnionBlob(route,
+			payment.PaymentHash[:])
 		if err != nil {
 			return preImage, nil, err
 		}
@@ -1002,13 +1016,14 @@ func (r *ChannelRouter) SendPayment(payment *LightningPayment) ([32]byte, *Route
 			Expiry:      route.TotalTimeLock,
 			PaymentHash: payment.PaymentHash,
 		}
-		copy(htlcAdd.OnionBlob[:], sphinxPacket)
+		copy(htlcAdd.OnionBlob[:], onionBlob)
 
 		// Attempt to send this payment through the network to complete
 		// the payment. If this attempt fails, then we'll continue on
 		// to the next available route.
 		firstHop := route.Hops[0].Channel.Node.PubKey
-		preImage, sendError = r.cfg.SendToSwitch(firstHop, htlcAdd)
+		preImage, sendError = r.cfg.SendToSwitch(firstHop, htlcAdd,
+			failureObfuscator)
 		if sendError != nil {
 			log.Errorf("Attempt to send payment %x failed: %v",
 				payment.PaymentHash, sendError)

backend-engineer1/land

lnd_test.go
@@ -2030,10 +2030,11 @@ out:
 	} else if resp.PaymentError == "" {
 		t.Fatalf("payment should have been rejected due to invalid " +
 			"payment hash")
-	} else if !strings.Contains(resp.PaymentError, "preimage") {
+	} else if !strings.Contains(resp.PaymentError,
+		lnwire.CodeUnknownPaymentHash.String()) {
 		// TODO(roasbeef): make into proper gRPC error code
-		t.Fatalf("payment should have failed due to unknown preimage, "+
-			"instead failed due to : %v", err)
+		t.Fatalf("payment should have failed due to unknown payment hash, "+
+			"instead failed due to: %v", resp.PaymentError)
 	}
 
 	// The balances of all parties should be the same as initially since
@@ -2066,9 +2067,10 @@ out:
 	} else if resp.PaymentError == "" {
 		t.Fatalf("payment should have been rejected due to wrong " +
 			"HTLC amount")
-	} else if !strings.Contains(resp.PaymentError, "htlc value") {
+	} else if !strings.Contains(resp.PaymentError,
+		lnwire.CodeIncorrectPaymentAmount.String()) {
 		t.Fatalf("payment should have failed due to wrong amount, "+
-			"instead failed due to: %v", err)
+			"instead failed due to: %v", resp.PaymentError)
 	}
 
 	// The balances of all parties should be the same as initially since
@@ -2129,9 +2131,10 @@ out:
 	} else if resp.PaymentError == "" {
 		t.Fatalf("payment should fail due to insufficient "+
 			"capacity: %v", err)
-	} else if !strings.Contains(resp.PaymentError, "capacity") {
+	} else if !strings.Contains(resp.PaymentError,
+		lnwire.CodeTemporaryChannelFailure.String()) {
 		t.Fatalf("payment should fail due to insufficient capacity, "+
-			"instead: %v", err)
+			"instead: %v", resp.PaymentError)
 	}
 
 	// For our final test, we'll ensure that if a target link isn't
@@ -2158,9 +2161,10 @@ out:
 		t.Fatalf("payment stream has been closed: %v", err)
 	} else if resp.PaymentError == "" {
 		t.Fatalf("payment should have failed")
-	} else if !strings.Contains(resp.PaymentError, "hop unknown") {
+	} else if !strings.Contains(resp.PaymentError,
+		lnwire.CodeUnknownNextPeer.String()) {
 		t.Fatalf("payment should fail due to unknown hop, instead: %v",
-			err)
+			resp.PaymentError)
 	}
 
 	// Finally, immediately close the channel. This function will also
@@ -2575,17 +2579,19 @@ func testAsyncPayments(net *networkHarness, t *harnessTest) {
 
 		if err := alicePayStream.Send(sendReq); err != nil {
 			t.Fatalf("unable to send payment: "+
-				"%v", err)
+				"stream has been closed: %v", err)
 		}
 	}
 
+	// We should receive one insufficient capacity error, because we are
+	// sending on one invoice bigger.
 	errorReceived := false
 	for i := 0; i < numInvoices; i++ {
 		if resp, err := alicePayStream.Recv(); err != nil {
 			t.Fatalf("payment stream have been closed: %v", err)
 		} else if resp.PaymentError != "" {
 			if strings.Contains(resp.PaymentError,
-				"Insufficient") {
+				lnwire.CodeTemporaryChannelFailure.String()) {
 				if errorReceived {
 					t.Fatalf("redundant payment "+
 						"error: %v", resp.PaymentError)
@@ -2595,7 +2601,7 @@ func testAsyncPayments(net *networkHarness, t *harnessTest) {
 				continue
 			}
 
-			t.Fatalf("unable to send payment: %v", err)
+			t.Fatalf("unable to send payment: %v", resp.PaymentError)
 		}
 	}
 

routing/router.go
@@ -17,6 +17,8 @@ import (
 	"github.com/roasbeef/btcd/wire"
 	"github.com/roasbeef/btcutil"
 
+	"crypto/sha256"
+
 	"github.com/go-errors/errors"
 	"github.com/lightningnetwork/lightning-onion"
 )
@@ -111,8 +113,8 @@ type Config struct {
 	// forward a fully encoded payment to the first hop in the route
 	// denoted by its public key. A non-nil error is to be returned if the
 	// payment was unsuccessful.
-	SendToSwitch func(firstHop *btcec.PublicKey,
-		htlcAdd *lnwire.UpdateAddHTLC) ([32]byte, error)
+	SendToSwitch func(firstHop *btcec.PublicKey, htlcAdd *lnwire.UpdateAddHTLC,
+		circuit *sphinx.Circuit) ([sha256.Size]byte, error)
 }
 
 // routeTuple is an entry within the ChannelRouter's route cache. We cache
@@ -851,7 +853,8 @@ func (r *ChannelRouter) FindRoutes(target *btcec.PublicKey, amt btcutil.Amount) 
 // the onion route specified by the passed layer 3 route. The blob returned
 // from this function can immediately be included within an HTLC add packet to
 // be sent to the first hop within the route.
-func generateSphinxPacket(route *Route, paymentHash []byte) ([]byte, error) {
+func generateSphinxPacket(route *Route, paymentHash []byte) ([]byte,
+	*sphinx.Circuit, error) {
 	// First obtain all the public keys along the route which are contained
 	// in each hop.
 	nodes := make([]*btcec.PublicKey, len(route.Hops))
@@ -877,7 +880,7 @@ func generateSphinxPacket(route *Route, paymentHash []byte) ([]byte, error) {
 
 	sessionKey, err := btcec.NewPrivateKey(btcec.S256())
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	// Next generate the onion routing packet which allows us to perform
@@ -885,14 +888,14 @@ func generateSphinxPacket(route *Route, paymentHash []byte) ([]byte, error) {
 	sphinxPacket, err := sphinx.NewOnionPacket(nodes, sessionKey,
 		hopPayloads, paymentHash)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	// Finally, encode Sphinx packet using it's wire representation to be
 	// included within the HTLC add packet.
 	var onionBlob bytes.Buffer
 	if err := sphinxPacket.Encode(&onionBlob); err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	log.Tracef("Generated sphinx packet: %v",
@@ -904,7 +907,10 @@ func generateSphinxPacket(route *Route, paymentHash []byte) ([]byte, error) {
 		}),
 	)
 
-	return onionBlob.Bytes(), nil
+	return onionBlob.Bytes(), &sphinx.Circuit{
+		SessionKey:  sessionKey,
+		PaymentPath: nodes,
+	}, nil
 }
 
 // LightningPayment describes a payment to be sent through the network to the
@@ -989,7 +995,8 @@ func (r *ChannelRouter) SendPayment(payment *LightningPayment) ([32]byte, *Route
 		// Generate the raw encoded sphinx packet to be included along
 		// with the htlcAdd message that we send directly to the
 		// switch.
-		sphinxPacket, err := generateSphinxPacket(route, payment.PaymentHash[:])
+		onionBlob, circuit, err := generateSphinxPacket(route,
+			payment.PaymentHash[:])
 		if err != nil {
 			return preImage, nil, err
 		}
@@ -1002,13 +1009,14 @@ func (r *ChannelRouter) SendPayment(payment *LightningPayment) ([32]byte, *Route
 			Expiry:      route.TotalTimeLock,
 			PaymentHash: payment.PaymentHash,
 		}
-		copy(htlcAdd.OnionBlob[:], sphinxPacket)
+		copy(htlcAdd.OnionBlob[:], onionBlob)
 
 		// Attempt to send this payment through the network to complete
 		// the payment. If this attempt fails, then we'll continue on
 		// to the next available route.
 		firstHop := route.Hops[0].Channel.Node.PubKey
-		preImage, sendError = r.cfg.SendToSwitch(firstHop, htlcAdd)
+		preImage, sendError = r.cfg.SendToSwitch(firstHop, htlcAdd,
+			circuit)
 		if sendError != nil {
 			log.Errorf("Attempt to send payment %x failed: %v",
 				payment.PaymentHash, sendError)