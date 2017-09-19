treygriffith/lnd

config+htlclink+peer: htlc hodl mode!
  This commit adds a new debug mode for lnd
  called hodlhtlc. This mode instructs a node
  to refrain from settling incoming HTLCs for
  which it is the exit node. We plan to use
  this in testing to more precisely control
  the states a node can take during
  execution.

config.go
@@ -113,6 +113,7 @@ type config struct {
 	RPCPort            int  `long:"rpcport" description:"The port for the rpc server"`
 	RESTPort           int  `long:"restport" description:"The port for the REST server"`
 	DebugHTLC          bool `long:"debughtlc" description:"Activate the debug htlc mode. With the debug HTLC mode, all payments sent use a pre-determined R-Hash. Additionally, all HTLCs sent to a node with the debug HTLC R-Hash are immediately settled in the next available state transition."`
+	HodlHTLC           bool `long:"hodlhtlc" description:"Activate the hodl HTLC mode.  With hodl HTLC mode, all incoming HTLCs will be accepted by the recieving node, but no attempt will be made to settle the payment with the sender."`
 	MaxPendingChannels int  `long:"maxpendingchannels" description:"The maximum number of incoming pending channels permitted per peer."`
 
 	Litecoin *chainConfig `group:"Litecoin" namespace:"litecoin"`

htlcswitch/link.go
@@ -132,6 +132,12 @@ type ChannelLinkConfig struct {
 	// with the debug htlc R-Hash are immediately settled in the next
 	// available state transition.
 	DebugHTLC bool
+
+	// HodlHTLC should be active if you want this node to refrain from
+	// settling all incoming HTLCs with the sender if it finds itself to be
+	// the exit node.
+	// NOTE: HodlHTLC should be active in conjunction with DebugHTLC.
+	HodlHTLC bool
 }
 
 // channelLink is the service which drives a channel's commitment update
@@ -1150,6 +1156,13 @@ func (l *channelLink) processLockedInHtlcs(
 					failure := lnwire.FailIncorrectPaymentAmount{}
 					l.sendHTLCError(pd.RHash, failure, obfuscator)
 					needUpdate = true
+					continue
+				}
+
+				if l.cfg.DebugHTLC && l.cfg.HodlHTLC {
+					log.Errorf("hodl HTLC mode enabled, " +
+						"will not attempt to settle " +
+						"HTLC with sender")
 					continue
 				}
 

backend-engineer1/land

config.go
@@ -113,6 +113,7 @@ type config struct {
 	RPCPort            int  `long:"rpcport" description:"The port for the rpc server"`
 	RESTPort           int  `long:"restport" description:"The port for the REST server"`
 	DebugHTLC          bool `long:"debughtlc" description:"Activate the debug htlc mode. With the debug HTLC mode, all payments sent use a pre-determined R-Hash. Additionally, all HTLCs sent to a node with the debug HTLC R-Hash are immediately settled in the next available state transition."`
+	HodlHTLC           bool `long:"hodlhtlc" description:"Activate the hodl HTLC mode.  With hodl HTLC mode, all incoming HTLCs will be accepted by the receiving node, but no attempt will be made to settle the payment with the sender."`
 	MaxPendingChannels int  `long:"maxpendingchannels" description:"The maximum number of incoming pending channels permitted per peer."`
 
 	Litecoin *chainConfig `group:"Litecoin" namespace:"litecoin"`

htlcswitch/link.go
@@ -132,6 +132,12 @@ type ChannelLinkConfig struct {
 	// with the debug htlc R-Hash are immediately settled in the next
 	// available state transition.
 	DebugHTLC bool
+
+	// HodlHTLC should be active if you want this node to refrain from
+	// settling all incoming HTLCs with the sender if it finds itself to be
+	// the exit node.
+	// NOTE: HodlHTLC should be active in conjunction with DebugHTLC.
+	HodlHTLC bool
 }
 
 // channelLink is the service which drives a channel's commitment update
@@ -1173,6 +1179,13 @@ func (l *channelLink) processLockedInHtlcs(
 					failure := lnwire.FailIncorrectPaymentAmount{}
 					l.sendHTLCError(pd.RHash, failure, obfuscator)
 					needUpdate = true
+					continue
+				}
+
+				if l.cfg.DebugHTLC && l.cfg.HodlHTLC {
+					log.Warnf("hodl HTLC mode enabled, " +
+						"will not attempt to settle " +
+						"HTLC with sender")
 					continue
 				}
 