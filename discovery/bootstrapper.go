lightningnetwork/lnd

torsvc: Added new module to house Tor functions
This commit adds a new module named 'torsvc' which houses all Tor
functionality in an attempt to isolate it and make it reusable in
other projecs. Some additional tweaks were made to config.go and
to the bootstrapper.

discovery/bootstrapper.go
@@ -237,8 +237,9 @@ func (c *ChannelGraphBootstrapper) Name() string {
 // boot strapping protocol, see this link:
 //     * https://github.com/lightningnetwork/lightning-rfc/blob/master/10-dns-bootstrap.md
 type DNSSeedBootstrapper struct {
-	dnsSeeds  []string
-	lookupFns []interface{}
+	dnsSeeds   []string
+	lookupHost func(string) ([]string, error)
+	lookupSRV  func(string, string, string) (string, []*net.SRV, error)
 }
 
 // A compile time assertion to ensure that DNSSeedBootstrapper meets the
@@ -248,12 +249,14 @@ var _ NetworkPeerBootstrapper = (*ChannelGraphBootstrapper)(nil)
 // NewDNSSeedBootstrapper returns a new instance of the DNSSeedBootstrapper.
 // The set of passed seeds should point to DNS servers that properly implement
 // Lighting's DNS peer bootstrapping protocol as defined in BOLT-0010.
-// The lookupFns slice contains the lndLookup, lndLookupSRV, & lndResolveTCP
-// functions respectively.
-func NewDNSSeedBootstrapper(seeds []string, lookupFns []interface{}) (NetworkPeerBootstrapper, error) {
+func NewDNSSeedBootstrapper(seeds []string, lookupHost func(string) ([]string, error),
+	lookupSRV func(string, string, string) (string, []*net.SRV, error)) (
+	NetworkPeerBootstrapper, error) {
+
 	return &DNSSeedBootstrapper{
-		dnsSeeds:  seeds,
-		lookupFns: lookupFns,
+		dnsSeeds:   seeds,
+		lookupHost: lookupHost,
+		lookupSRV:  lookupSRV,
 	}, nil
 }
 
@@ -276,7 +279,7 @@ search:
 			// can obtain a random sample of the encoded public
 			// keys of nodes. We use the lndLookupSRV function for
 			// this task.
-			_, addrs, err := d.lookupFns[1].(func(string, string, string) (string, []*net.SRV, error))("nodes", "tcp", dnsSeed)
+			_, addrs, err := d.lookupSRV("nodes", "tcp", dnsSeed)
 			if err != nil {
 				return nil, err
 			}
@@ -298,7 +301,7 @@ search:
 				// key. We use the lndLookup function for this
 				// task.
 				bechNodeHost := nodeSrv.Target
-				addrs, err := d.lookupFns[0].(func(string) ([]string, error))(bechNodeHost)
+				addrs, err := d.lookupHost(bechNodeHost)
 				if err != nil {
 					return nil, err
 				}

torsvc/torsvc.go
@@ -1,0 +1,162 @@
+package torsvc
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+
+	"github.com/btcsuite/btcd/connmgr"
+	"github.com/btcsuite/go-socks/socks"
+	"github.com/miekg/dns"
+	"golang.org/x/net/proxy"
+)
+
+const (
+	localhost = "127.0.0.1"
+)
+
+var (
+	// DNS Message Response Codes, see
+	// https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml
+	dnsCodes = map[int]string{
+		0:  "No Error",
+		1:  "Format Error",
+		2:  "Server Failure",
+		3:  "Non-Existent Domain",
+		4:  "Not Implemented",
+		5:  "Query Refused",
+		6:  "Name Exists when it should not",
+		7:  "RR Set Exists when it should not",
+		8:  "RR Set that should exist does not",
+		9:  "Server Not Authoritative for zone",
+		10: "Name not contained in zone",
+		// Left out 16: "Bad OPT Version" because of duplicate keys and
+		// because miekg/dns does not use this message response code.
+		16: "TSIG Signature Failure",
+		17: "Key not recognized",
+		18: "Signature out of time window",
+		19: "Bad TKEY Mode",
+		20: "Duplicate key name",
+		21: "Algorithm not supported",
+		22: "Bad Truncation",
+		23: "Bad/missing Server Cookie",
+	}
+)
+
+// TorDial returns a go-socks proxy dialer function that can be used to make
+// connections to peers through Tor's exposed socks proxy.
+func TorDial(socksPort string) func(string, string) (net.Conn, error) {
+	p := &socks.Proxy{Addr: localhost + ":" + socksPort}
+	return p.Dial
+}
+
+// TorLookupHost performs DNS resolution on a given hostname via Tor's
+// native resolver. Only IPv4 addresses are returned.
+func TorLookupHost(host, socksPort string) ([]string, error) {
+	ip, err := connmgr.TorLookupIP(host, localhost+":"+socksPort)
+	if err != nil {
+		return nil, err
+	}
+
+	var addrs []string
+	// Only one IPv4 address is returned by the TorLookupIP function.
+	addrs = append(addrs, ip[0].String())
+	return addrs, nil
+}
+
+// TorLookupSRV uses Tor's socks proxy to route DNS SRV queries. Tor does not
+// natively support SRV queries so we must route all SRV queries THROUGH the
+// Tor proxy and connect directly to a DNS server and query it.
+// NOTE: TorLookupSRV uses golang's proxy package since go-socks will cause
+// the SRV request to hang.
+func TorLookupSRV(service, proto, name, socksPort, dnsServer string) (string,
+	[]*net.SRV, error) {
+	// _service._proto.name as described in RFC#2782.
+	host := "_" + service + "._" + proto + "." + name + "."
+
+	// Set up golang's proxy dialer - Tor's socks proxy doesn't support
+	// authentication.
+	dialer, err := proxy.SOCKS5(
+		"tcp",
+		localhost+":"+socksPort,
+		nil,
+		proxy.Direct,
+	)
+	if err != nil {
+		return "", nil, err
+	}
+
+	// Dial dnsServer via Tor. dnsServer must have TCP resolution enabled
+	// for the port we are dialing.
+	conn, err := dialer.Dial("tcp", dnsServer)
+	if err != nil {
+		return "", nil, err
+	}
+
+	// Construct the actual SRV request.
+	msg := new(dns.Msg)
+	msg.SetQuestion(host, dns.TypeSRV)
+	msg.RecursionDesired = true
+
+	dnsConn := &dns.Conn{Conn: conn}
+	defer dnsConn.Close()
+
+	// Write the SRV request.
+	dnsConn.WriteMsg(msg)
+
+	// Read the response.
+	resp, err := dnsConn.ReadMsg()
+	if err != nil {
+		return "", nil, err
+	}
+
+	// If the message response code was not the success code, fail.
+	if resp.Rcode != dns.RcodeSuccess {
+		return "", nil, fmt.Errorf("Unsuccessful SRV request, "+
+			"received: %s", dnsCodes[resp.Rcode])
+	}
+
+	// Retrieve the RR(s) of the Answer section.
+	var rrs []*net.SRV
+	for _, rr := range resp.Answer {
+		srv := rr.(*dns.SRV)
+		rrs = append(rrs, &net.SRV{
+			Target:   srv.Target,
+			Port:     srv.Port,
+			Priority: srv.Priority,
+			Weight:   srv.Weight,
+		})
+	}
+
+	return "", rrs, nil
+}
+
+// TorResolveTCP uses Tor's proxy to resolve TCP addresses instead of the
+// system resolver that ResolveTCPAddr and related functions use. This resolver
+// only queries DNS servers in the case that a hostname is passed in the
+// address parameter. Only TCP resolution is supported.
+func TorResolveTCP(address, socksPort string) (*net.TCPAddr, error) {
+	// Split host:port since the lookup function does not take a port.
+	host, port, err := net.SplitHostPort(address)
+	if err != nil {
+		return nil, err
+	}
+
+	// Look up the host's IP address via Tor.
+	ip, err := TorLookupHost(host, socksPort)
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert port to an int.
+	p, err := strconv.Atoi(port)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return a *net.TCPAddr exactly like net.ResolveTCPAddr.
+	return &net.TCPAddr{
+		IP:   net.ParseIP(ip[0]),
+		Port: p,
+	}, nil
+}

backend-engineer1/land

discovery/bootstrapper.go
@@ -246,8 +246,9 @@ type DNSSeedBootstrapper struct {
 	// in the tuple is a special A record that we'll query in order to
 	// receive the IP address of the current authoritative DNS server for
 	// the network seed.
-	dnsSeeds [][2]string
-	lookupFns []interface{}
+	dnsSeeds   [][2]string
+	lookupHost func(string) ([]string, error)
+	lookupSRV  func(string, string, string) (string, []*net.SRV, error)
 }
 
 // A compile time assertion to ensure that DNSSeedBootstrapper meets the
@@ -261,10 +262,13 @@ var _ NetworkPeerBootstrapper = (*ChannelGraphBootstrapper)(nil)
 // used as a fallback for manual TCP resolution in the case of an error
 // receiving the UDP response. The second host should return a single A record
 // with the IP address of the authoritative name server.
-func NewDNSSeedBootstrapper(seeds [][2]string, lookupFns []interface{}) (NetworkPeerBootstrapper, error) {
+func NewDNSSeedBootstrapper(seeds [][2]string, lookupHost func(string) ([]string, error),
+	lookupSRV func(string, string, string) (string, []*net.SRV, error)) (
+	NetworkPeerBootstrapper, error) {
 	return &DNSSeedBootstrapper{
-		dnsSeeds:  seeds,
-		lookupFns: lookupFns,
+		dnsSeeds:   seeds,
+		lookupHost: lookupHost,
+		lookupSRV:  lookupSRV,
 	}, nil
 }
 
@@ -351,7 +355,7 @@ search:
 			// keys of nodes. We use the lndLookupSRV function for
 			// this task.
 			primarySeed := dnsSeedTuple[0]
-			_, addrs, err := d.lookupFns[1].(func(string, string, string) (string, []*net.SRV, error))("nodes", "tcp", primarySeed)
+			_, addrs, err := d.lookupSRV("nodes", "tcp", primarySeed)
 			if err != nil {
 				log.Tracef("Unable to lookup SRV records via " +
 					"primary seed, falling back to secondary")
@@ -390,7 +394,7 @@ search:
 				// key. We use the lndLookup function for this
 				// task.
 				bechNodeHost := nodeSrv.Target
-				addrs, err := d.lookupFns[0].(func(string) ([]string, error))(bechNodeHost)
+				addrs, err := d.lookupHost(bechNodeHost)
 				if err != nil {
 					return nil, err
 				}

torsvc/torsvc.go
@@ -1,0 +1,162 @@
+package torsvc
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+
+	"github.com/btcsuite/go-socks/socks"
+	"github.com/miekg/dns"
+	"github.com/roasbeef/btcd/connmgr"
+	"golang.org/x/net/proxy"
+)
+
+const (
+	localhost = "127.0.0.1"
+)
+
+var (
+	// DNS Message Response Codes, see
+	// https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml
+	dnsCodes = map[int]string{
+		0:  "No Error",
+		1:  "Format Error",
+		2:  "Server Failure",
+		3:  "Non-Existent Domain",
+		4:  "Not Implemented",
+		5:  "Query Refused",
+		6:  "Name Exists when it should not",
+		7:  "RR Set Exists when it should not",
+		8:  "RR Set that should exist does not",
+		9:  "Server Not Authoritative for zone",
+		10: "Name not contained in zone",
+		// Left out 16: "Bad OPT Version" because of duplicate keys and
+		// because miekg/dns does not use this message response code.
+		16: "TSIG Signature Failure",
+		17: "Key not recognized",
+		18: "Signature out of time window",
+		19: "Bad TKEY Mode",
+		20: "Duplicate key name",
+		21: "Algorithm not supported",
+		22: "Bad Truncation",
+		23: "Bad/missing Server Cookie",
+	}
+)
+
+// TorDial returns a go-socks proxy dialer function that can be used to make
+// connections to peers through Tor's exposed socks proxy.
+func TorDial(socksPort string) func(string, string) (net.Conn, error) {
+	p := &socks.Proxy{Addr: localhost + ":" + socksPort}
+	return p.Dial
+}
+
+// TorLookupHost performs DNS resolution on a given hostname via Tor's
+// native resolver. Only IPv4 addresses are returned.
+func TorLookupHost(host, socksPort string) ([]string, error) {
+	ip, err := connmgr.TorLookupIP(host, localhost+":"+socksPort)
+	if err != nil {
+		return nil, err
+	}
+
+	var addrs []string
+	// Only one IPv4 address is returned by the TorLookupIP function.
+	addrs = append(addrs, ip[0].String())
+	return addrs, nil
+}
+
+// TorLookupSRV uses Tor's socks proxy to route DNS SRV queries. Tor does not
+// natively support SRV queries so we must route all SRV queries THROUGH the
+// Tor proxy and connect directly to a DNS server and query it.
+// NOTE: TorLookupSRV uses golang's proxy package since go-socks will cause
+// the SRV request to hang.
+func TorLookupSRV(service, proto, name, socksPort, dnsServer string) (string,
+	[]*net.SRV, error) {
+	// _service._proto.name as described in RFC#2782.
+	host := "_" + service + "._" + proto + "." + name + "."
+
+	// Set up golang's proxy dialer - Tor's socks proxy doesn't support
+	// authentication.
+	dialer, err := proxy.SOCKS5(
+		"tcp",
+		localhost+":"+socksPort,
+		nil,
+		proxy.Direct,
+	)
+	if err != nil {
+		return "", nil, err
+	}
+
+	// Dial dnsServer via Tor. dnsServer must have TCP resolution enabled
+	// for the port we are dialing.
+	conn, err := dialer.Dial("tcp", dnsServer)
+	if err != nil {
+		return "", nil, err
+	}
+
+	// Construct the actual SRV request.
+	msg := new(dns.Msg)
+	msg.SetQuestion(host, dns.TypeSRV)
+	msg.RecursionDesired = true
+
+	dnsConn := &dns.Conn{Conn: conn}
+	defer dnsConn.Close()
+
+	// Write the SRV request.
+	dnsConn.WriteMsg(msg)
+
+	// Read the response.
+	resp, err := dnsConn.ReadMsg()
+	if err != nil {
+		return "", nil, err
+	}
+
+	// If the message response code was not the success code, fail.
+	if resp.Rcode != dns.RcodeSuccess {
+		return "", nil, fmt.Errorf("Unsuccessful SRV request, "+
+			"received: %s", dnsCodes[resp.Rcode])
+	}
+
+	// Retrieve the RR(s) of the Answer section.
+	var rrs []*net.SRV
+	for _, rr := range resp.Answer {
+		srv := rr.(*dns.SRV)
+		rrs = append(rrs, &net.SRV{
+			Target:   srv.Target,
+			Port:     srv.Port,
+			Priority: srv.Priority,
+			Weight:   srv.Weight,
+		})
+	}
+
+	return "", rrs, nil
+}
+
+// TorResolveTCP uses Tor's proxy to resolve TCP addresses instead of the
+// system resolver that ResolveTCPAddr and related functions use. This resolver
+// only queries DNS servers in the case that a hostname is passed in the
+// address parameter. Only TCP resolution is supported.
+func TorResolveTCP(address, socksPort string) (*net.TCPAddr, error) {
+	// Split host:port since the lookup function does not take a port.
+	host, port, err := net.SplitHostPort(address)
+	if err != nil {
+		return nil, err
+	}
+
+	// Look up the host's IP address via Tor.
+	ip, err := TorLookupHost(host, socksPort)
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert port to an int.
+	p, err := strconv.Atoi(port)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return a *net.TCPAddr exactly like net.ResolveTCPAddr.
+	return &net.TCPAddr{
+		IP:   net.ParseIP(ip[0]),
+		Port: p,
+	}, nil
+}