joostjager/lnd

invoices+htlcswitch+lnrpc: cancel invoice

htlcswitch/interfaces.go
@@ -19,6 +19,10 @@ type InvoiceDatabase interface {
 	// SettleInvoice attempts to mark an invoice corresponding to the
 	// passed payment hash as fully settled.
 	SettleInvoice(payHash chainhash.Hash, paidAmount lnwire.MilliSatoshi) error
+
+	// CancelInvoice attempts to cancel the invoice corresponding to the
+	// passed payment hash.
+	CancelInvoice(payHash chainhash.Hash) error
 }
 
 // ChannelLink is an interface which represents the subsystem for managing the

htlcswitch/link.go
@@ -2317,6 +2317,21 @@ func (l *channelLink) processRemoteAdds(fwdPkg *channeldb.FwdPkg,
 				continue
 			}
 
+			// Reject htlcs for canceled invoices.
+			if invoice.Terms.State == channeldb.ContractCanceled {
+				log.Errorf("rejecting htlc due to canceled " +
+					"invoice")
+
+				failure := lnwire.FailUnknownPaymentHash{}
+				l.sendHTLCError(
+					pd.HtlcIndex, failure, obfuscator,
+					pd.SourceRef,
+				)
+
+				needUpdate = true
+				continue
+			}
+
 			// If the invoice is already settled, we choose to
 			// accept the payment to simplify failure recovery.
 			//

invoices/invoiceregistry.go
@@ -163,7 +163,11 @@ func (i *InvoiceRegistry) invoiceEventNotifier() {
 		// A sub-systems has just modified the invoice state, so we'll
 		// dispatch notifications to all registered clients.
 		case event := <-i.invoiceEvents:
-			i.dispatchToClients(event)
+			// For backwards compatibility, do not notify all
+			// invoice subscribers of cancel events
+			if event.state != channeldb.ContractCanceled {
+				i.dispatchToClients(event)
+			}
 			i.dispatchToSingleClients(event)
 
 		case <-i.quit:
@@ -257,7 +261,7 @@ func (i *InvoiceRegistry) dispatchToClients(event *invoiceEvent) {
 		case channeldb.ContractOpen:
 			client.addIndex = invoice.AddIndex
 		default:
-			log.Errorf("unknown invoice state: %v", event.state)
+			log.Errorf("unexpected invoice state: %v", event.state)
 		}
 	}
 }
@@ -471,6 +475,34 @@ func (i *InvoiceRegistry) SettleInvoice(rHash chainhash.Hash,
 	log.Infof("Payment received: %v", spew.Sdump(invoice))
 
 	i.notifyClients(rHash, invoice, channeldb.ContractSettled)
+
+	return nil
+}
+
+// CancelInvoice attempts to cancel the invoice corresponding to the passed
+// payment hash.
+func (i *InvoiceRegistry) CancelInvoice(payHash chainhash.Hash) error {
+	i.Lock()
+	defer i.Unlock()
+
+	log.Debugf("Canceling invoice %x", payHash[:])
+
+	invoice, err := i.cdb.CancelInvoice(payHash)
+
+	// Implement idempotency by returning success if the invoice was already
+	// canceled.
+	if err == channeldb.ErrInvoiceAlreadyCanceled {
+		log.Debugf("Invoice %x already canceled", payHash[:])
+		return nil
+	}
+
+	if err != nil {
+		return err
+	}
+
+	log.Infof("Invoice canceled: %v", spew.Sdump(invoice))
+
+	i.notifyClients(payHash, invoice, channeldb.ContractCanceled)
 
 	return nil
 }

backend-engineer1/land

htlcswitch/interfaces.go
@@ -20,6 +20,10 @@ type InvoiceDatabase interface {
 	// SettleInvoice attempts to mark an invoice corresponding to the
 	// passed payment hash as fully settled.
 	SettleInvoice(payHash lntypes.Hash, paidAmount lnwire.MilliSatoshi) error
+
+	// CancelInvoice attempts to cancel the invoice corresponding to the
+	// passed payment hash.
+	CancelInvoice(payHash lntypes.Hash) error
 }
 
 // ChannelLink is an interface which represents the subsystem for managing the

htlcswitch/link.go
@@ -2337,6 +2337,23 @@ func (l *channelLink) processRemoteAdds(fwdPkg *channeldb.FwdPkg,
 				continue
 			}
 
+			// Reject htlcs for canceled invoices.
+			if invoice.Terms.State == channeldb.ContractCanceled {
+				l.errorf("Rejecting htlc due to canceled " +
+					"invoice")
+
+				failure := lnwire.NewFailUnknownPaymentHash(
+					pd.Amount,
+				)
+				l.sendHTLCError(
+					pd.HtlcIndex, failure, obfuscator,
+					pd.SourceRef,
+				)
+
+				needUpdate = true
+				continue
+			}
+
 			// If the invoice is already settled, we choose to
 			// accept the payment to simplify failure recovery.
 			//

invoices/invoiceregistry.go
@@ -162,7 +162,11 @@ func (i *InvoiceRegistry) invoiceEventNotifier() {
 		// A sub-systems has just modified the invoice state, so we'll
 		// dispatch notifications to all registered clients.
 		case event := <-i.invoiceEvents:
-			i.dispatchToClients(event)
+			// For backwards compatibility, do not notify all
+			// invoice subscribers of cancel events
+			if event.state != channeldb.ContractCanceled {
+				i.dispatchToClients(event)
+			}
 			i.dispatchToSingleClients(event)
 
 		case <-i.quit:
@@ -256,7 +260,7 @@ func (i *InvoiceRegistry) dispatchToClients(event *invoiceEvent) {
 		case channeldb.ContractOpen:
 			client.addIndex = invoice.AddIndex
 		default:
-			log.Errorf("unknown invoice state: %v", event.state)
+			log.Errorf("unexpected invoice state: %v", event.state)
 		}
 	}
 }
@@ -472,6 +476,34 @@ func (i *InvoiceRegistry) SettleInvoice(rHash lntypes.Hash,
 	log.Infof("Payment received: %v", spew.Sdump(invoice))
 
 	i.notifyClients(rHash, invoice, channeldb.ContractSettled)
+
+	return nil
+}
+
+// CancelInvoice attempts to cancel the invoice corresponding to the passed
+// payment hash.
+func (i *InvoiceRegistry) CancelInvoice(payHash lntypes.Hash) error {
+	i.Lock()
+	defer i.Unlock()
+
+	log.Debugf("Canceling invoice %v", payHash)
+
+	invoice, err := i.cdb.CancelInvoice(payHash)
+
+	// Implement idempotency by returning success if the invoice was already
+	// canceled.
+	if err == channeldb.ErrInvoiceAlreadyCanceled {
+		log.Debugf("Invoice %v already canceled", payHash)
+		return nil
+	}
+
+	if err != nil {
+		return err
+	}
+
+	log.Infof("Invoice %v canceled", payHash)
+
+	i.notifyClients(payHash, invoice, channeldb.ContractCanceled)
 
 	return nil
 }