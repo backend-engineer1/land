joostjager/lnd

htlcswitch+routing+routerrpc: return error source as index

lnrpc/routerrpc/router_server.go
@@ -399,7 +399,7 @@ func marshallError(sendError error) (*Failure, error) {
 		return nil, errors.New("unknown wire error")
 	}
 
-	response.FailureSourcePubkey = fErr.ErrorSource.SerializeCompressed()
+	response.FailureSourceIndex = int32(fErr.FailureSourceIdx)
 
 	return response, nil
 }

backend-engineer1/land

lnrpc/routerrpc/router_server.go
@@ -399,7 +399,7 @@ func marshallError(sendError error) (*Failure, error) {
 		return nil, errors.New("unknown wire error")
 	}
 
-	response.FailureSourcePubkey = fErr.ErrorSource.SerializeCompressed()
+	response.FailureSourceIndex = uint32(fErr.FailureSourceIdx)
 
 	return response, nil
 }