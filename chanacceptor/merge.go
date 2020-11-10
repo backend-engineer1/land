gitlikeagirl/lnd

multi: add channel open parameters to channel acceptor
Add more fields to channel acceptor response so that users can have more
fine grained control over their incoming channels. With our chained
acceptor, it is possible that we get inconsistent responses from
multiple chained acceptors. We create a conjugate repsponse from all the
set fields in our various responses, but fail if we get different, non-
zero responses from our various acceptors. Separate merge functions are
used per type so that we avoid unexpected outcomes comparing interfaces
(panic on comparing types that aren't comparable), with casting used
where applicable to avoid code duplication.

chanacceptor/merge.go
@@ -1,0 +1,153 @@
+package chanacceptor
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/btcsuite/btcutil"
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+const (
+	// We use field names in our errors for more readable errors. Create
+	// consts for them here so that we can exactly match in our unit tests.
+	fieldCSV             = "csv delay"
+	fieldHtlcLimit       = "htlc limit"
+	fieldMinDep          = "min depth"
+	fieldReserve         = "reserve"
+	fieldMinIn           = "min htlc in"
+	fieldInFlightTotal   = "in flight total"
+	fieldUpfrontShutdown = "upfront shutdown"
+)
+
+// mergeError returns a merge error for a named field when we get two channel
+// acceptor responses which have different values set.
+func mergeError(name string, current, new interface{}) error {
+	return fmt.Errorf("multiple values set for: %v, %v and %v",
+		name, current, new)
+}
+
+// mergeInt64 merges two int64 values, failing if they have different non-zero
+// values.
+func mergeInt64(name string, current, new int64) (int64, error) {
+	switch {
+	case current == 0:
+		return new, nil
+
+	case new == 0:
+		return current, nil
+
+	case current != new:
+		return 0, mergeError(name, current, new)
+
+	default:
+		return new, nil
+	}
+}
+
+// mergeMillisatoshi merges two msat values, failing if they have different
+// non-zero values.
+func mergeMillisatoshi(name string, current,
+	new lnwire.MilliSatoshi) (lnwire.MilliSatoshi, error) {
+
+	switch {
+	case current == 0:
+		return new, nil
+
+	case new == 0:
+		return current, nil
+
+	case current != new:
+		return 0, mergeError(name, current, new)
+
+	default:
+		return new, nil
+	}
+}
+
+// mergeDeliveryAddress merges two delivery address values, failing if they have
+// different non-zero values.
+func mergeDeliveryAddress(name string, current,
+	new lnwire.DeliveryAddress) (lnwire.DeliveryAddress, error) {
+
+	switch {
+	case current == nil:
+		return new, nil
+
+	case new == nil:
+		return current, nil
+
+	case !bytes.Equal(current, new):
+		return nil, mergeError(name, current, new)
+
+	default:
+		return new, nil
+	}
+}
+
+// mergeResponse takes two channel accept responses, and attempts to merge their
+// fields, failing if any fields conflict (are non-zero and not equal). It
+// returns a new response that has all the merged fields in it.
+func mergeResponse(current, new ChannelAcceptResponse) (ChannelAcceptResponse,
+	error) {
+
+	csv, err := mergeInt64(
+		fieldCSV, int64(current.CSVDelay), int64(new.CSVDelay),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.CSVDelay = uint16(csv)
+
+	htlcLimit, err := mergeInt64(
+		fieldHtlcLimit, int64(current.HtlcLimit),
+		int64(new.HtlcLimit),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.HtlcLimit = uint16(htlcLimit)
+
+	minDepth, err := mergeInt64(
+		fieldMinDep, int64(current.MinAcceptDepth),
+		int64(new.MinAcceptDepth),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.MinAcceptDepth = uint16(minDepth)
+
+	reserve, err := mergeInt64(
+		fieldReserve, int64(current.Reserve), int64(new.Reserve),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.Reserve = btcutil.Amount(reserve)
+
+	// Merge msat values and set directly.
+	current.MinHtlcIn, err = mergeMillisatoshi(
+		fieldMinIn, current.MinHtlcIn, new.MinHtlcIn,
+	)
+	if err != nil {
+		return current, err
+	}
+
+	current.InFlightTotal, err = mergeMillisatoshi(
+		fieldInFlightTotal, current.InFlightTotal,
+		new.InFlightTotal,
+	)
+	if err != nil {
+		return current, err
+	}
+
+	current.UpfrontShutdown, err = mergeDeliveryAddress(
+		fieldUpfrontShutdown, current.UpfrontShutdown,
+		new.UpfrontShutdown,
+	)
+	if err != nil {
+		return current, err
+	}
+
+	return current, nil
+}

chanacceptor/merge_test.go
@@ -1,0 +1,188 @@
+package chanacceptor
+
+import (
+	"testing"
+
+	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/stretchr/testify/require"
+)
+
+// TestMergeResponse tests merging of channel acceptor responses.
+func TestMergeResponse(t *testing.T) {
+	var (
+		addr1 = lnwire.DeliveryAddress{1}
+		addr2 = lnwire.DeliveryAddress{2}
+
+		populatedResp = ChannelAcceptResponse{
+			UpfrontShutdown: addr1,
+			CSVDelay:        2,
+			Reserve:         3,
+			InFlightTotal:   4,
+			HtlcLimit:       5,
+			MinHtlcIn:       6,
+			MinAcceptDepth:  7,
+		}
+	)
+
+	tests := []struct {
+		name    string
+		current ChannelAcceptResponse
+		new     ChannelAcceptResponse
+		merged  ChannelAcceptResponse
+		err     error
+	}{
+		{
+			name:    "same response",
+			current: populatedResp,
+			new:     populatedResp,
+			merged:  populatedResp,
+			err:     nil,
+		},
+		{
+			name: "different upfront",
+			current: ChannelAcceptResponse{
+				UpfrontShutdown: addr1,
+			},
+			new: ChannelAcceptResponse{
+				UpfrontShutdown: addr2,
+			},
+			err: mergeError(fieldUpfrontShutdown, addr1, addr2),
+		},
+		{
+			name: "different csv",
+			current: ChannelAcceptResponse{
+				CSVDelay: 1,
+			},
+			new: ChannelAcceptResponse{
+				CSVDelay: 2,
+			},
+			err: mergeError(fieldCSV, 1, 2),
+		},
+		{
+			name: "different reserve",
+			current: ChannelAcceptResponse{
+				Reserve: 1,
+			},
+			new: ChannelAcceptResponse{
+				Reserve: 2,
+			},
+			err: mergeError(fieldReserve, 1, 2),
+		},
+		{
+			name: "different in flight",
+			current: ChannelAcceptResponse{
+				InFlightTotal: 1,
+			},
+			new: ChannelAcceptResponse{
+				InFlightTotal: 2,
+			},
+			err: mergeError(
+				fieldInFlightTotal, lnwire.MilliSatoshi(1),
+				lnwire.MilliSatoshi(2),
+			),
+		},
+		{
+			name: "different htlc limit",
+			current: ChannelAcceptResponse{
+				HtlcLimit: 1,
+			},
+			new: ChannelAcceptResponse{
+				HtlcLimit: 2,
+			},
+			err: mergeError(fieldHtlcLimit, 1, 2),
+		},
+		{
+			name: "different min in",
+			current: ChannelAcceptResponse{
+				MinHtlcIn: 1,
+			},
+			new: ChannelAcceptResponse{
+				MinHtlcIn: 2,
+			},
+			err: mergeError(
+				fieldMinIn, lnwire.MilliSatoshi(1),
+				lnwire.MilliSatoshi(2),
+			),
+		},
+		{
+			name: "different depth",
+			current: ChannelAcceptResponse{
+				MinAcceptDepth: 1,
+			},
+			new: ChannelAcceptResponse{
+				MinAcceptDepth: 2,
+			},
+			err: mergeError(fieldMinDep, 1, 2),
+		},
+		{
+			name: "merge all values",
+			current: ChannelAcceptResponse{
+				UpfrontShutdown: lnwire.DeliveryAddress{1},
+				CSVDelay:        1,
+				Reserve:         0,
+				InFlightTotal:   3,
+				HtlcLimit:       0,
+				MinHtlcIn:       5,
+				MinAcceptDepth:  0,
+			},
+			new: ChannelAcceptResponse{
+				UpfrontShutdown: nil,
+				CSVDelay:        0,
+				Reserve:         2,
+				InFlightTotal:   0,
+				HtlcLimit:       4,
+				MinHtlcIn:       0,
+				MinAcceptDepth:  6,
+			},
+			merged: ChannelAcceptResponse{
+				UpfrontShutdown: lnwire.DeliveryAddress{1},
+				CSVDelay:        1,
+				Reserve:         2,
+				InFlightTotal:   3,
+				HtlcLimit:       4,
+				MinHtlcIn:       5,
+				MinAcceptDepth:  6,
+			},
+			err: nil,
+		},
+		{
+			// Test the case where fields have the same non-zero
+			// value, and the case where only response value is
+			// non-zero.
+			name: "empty and identical",
+			current: ChannelAcceptResponse{
+				CSVDelay:      1,
+				Reserve:       2,
+				InFlightTotal: 0,
+			},
+			new: ChannelAcceptResponse{
+				CSVDelay:      0,
+				Reserve:       2,
+				InFlightTotal: 3,
+			},
+			merged: ChannelAcceptResponse{
+				CSVDelay:      1,
+				Reserve:       2,
+				InFlightTotal: 3,
+			},
+			err: nil,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+
+		t.Run(test.name, func(t *testing.T) {
+			resp, err := mergeResponse(test.current, test.new)
+			require.Equal(t, test.err, err)
+
+			// If we expect an error, exit early rather than compare
+			// our result.
+			if test.err != nil {
+				return
+			}
+
+			require.Equal(t, test.merged, resp)
+		})
+	}
+}

backend-engineer1/land

chanacceptor/merge.go
@@ -1,0 +1,152 @@
+package chanacceptor
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/btcsuite/btcutil"
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+const (
+	// We use field names in our errors for more readable errors. Create
+	// consts for them here so that we can exactly match in our unit tests.
+	fieldCSV             = "csv delay"
+	fieldHtlcLimit       = "htlc limit"
+	fieldMinDep          = "min depth"
+	fieldReserve         = "reserve"
+	fieldMinIn           = "min htlc in"
+	fieldInFlightTotal   = "in flight total"
+	fieldUpfrontShutdown = "upfront shutdown"
+)
+
+// fieldMismatchError returns a merge error for a named field when we get two
+// channel acceptor responses which have different values set.
+func fieldMismatchError(name string, current, new interface{}) error {
+	return fmt.Errorf("multiple values set for: %v, %v and %v",
+		name, current, new)
+}
+
+// mergeInt64 merges two int64 values, failing if they have different non-zero
+// values.
+func mergeInt64(name string, current, new int64) (int64, error) {
+	switch {
+	case current == 0:
+		return new, nil
+
+	case new == 0:
+		return current, nil
+
+	case current != new:
+		return 0, fieldMismatchError(name, current, new)
+
+	default:
+		return new, nil
+	}
+}
+
+// mergeMillisatoshi merges two msat values, failing if they have different
+// non-zero values.
+func mergeMillisatoshi(name string, current,
+	new lnwire.MilliSatoshi) (lnwire.MilliSatoshi, error) {
+
+	switch {
+	case current == 0:
+		return new, nil
+
+	case new == 0:
+		return current, nil
+
+	case current != new:
+		return 0, fieldMismatchError(name, current, new)
+
+	default:
+		return new, nil
+	}
+}
+
+// mergeDeliveryAddress merges two delivery address values, failing if they have
+// different non-zero values.
+func mergeDeliveryAddress(name string, current,
+	new lnwire.DeliveryAddress) (lnwire.DeliveryAddress, error) {
+
+	switch {
+	case current == nil:
+		return new, nil
+
+	case new == nil:
+		return current, nil
+
+	case !bytes.Equal(current, new):
+		return nil, fieldMismatchError(name, current, new)
+
+	default:
+		return new, nil
+	}
+}
+
+// mergeResponse takes two channel accept responses, and attempts to merge their
+// fields, failing if any fields conflict (are non-zero and not equal). It
+// returns a new response that has all the merged fields in it.
+func mergeResponse(current, new ChannelAcceptResponse) (ChannelAcceptResponse,
+	error) {
+
+	csv, err := mergeInt64(
+		fieldCSV, int64(current.CSVDelay), int64(new.CSVDelay),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.CSVDelay = uint16(csv)
+
+	htlcLimit, err := mergeInt64(
+		fieldHtlcLimit, int64(current.HtlcLimit),
+		int64(new.HtlcLimit),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.HtlcLimit = uint16(htlcLimit)
+
+	minDepth, err := mergeInt64(
+		fieldMinDep, int64(current.MinAcceptDepth),
+		int64(new.MinAcceptDepth),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.MinAcceptDepth = uint16(minDepth)
+
+	reserve, err := mergeInt64(
+		fieldReserve, int64(current.Reserve), int64(new.Reserve),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.Reserve = btcutil.Amount(reserve)
+
+	current.MinHtlcIn, err = mergeMillisatoshi(
+		fieldMinIn, current.MinHtlcIn, new.MinHtlcIn,
+	)
+	if err != nil {
+		return current, err
+	}
+
+	current.InFlightTotal, err = mergeMillisatoshi(
+		fieldInFlightTotal, current.InFlightTotal,
+		new.InFlightTotal,
+	)
+	if err != nil {
+		return current, err
+	}
+
+	current.UpfrontShutdown, err = mergeDeliveryAddress(
+		fieldUpfrontShutdown, current.UpfrontShutdown,
+		new.UpfrontShutdown,
+	)
+	if err != nil {
+		return current, err
+	}
+
+	return current, nil
+}

chanacceptor/merge_test.go
@@ -1,0 +1,188 @@
+package chanacceptor
+
+import (
+	"testing"
+
+	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/stretchr/testify/require"
+)
+
+// TestMergeResponse tests merging of channel acceptor responses.
+func TestMergeResponse(t *testing.T) {
+	var (
+		addr1 = lnwire.DeliveryAddress{1}
+		addr2 = lnwire.DeliveryAddress{2}
+
+		populatedResp = ChannelAcceptResponse{
+			UpfrontShutdown: addr1,
+			CSVDelay:        2,
+			Reserve:         3,
+			InFlightTotal:   4,
+			HtlcLimit:       5,
+			MinHtlcIn:       6,
+			MinAcceptDepth:  7,
+		}
+	)
+
+	tests := []struct {
+		name    string
+		current ChannelAcceptResponse
+		new     ChannelAcceptResponse
+		merged  ChannelAcceptResponse
+		err     error
+	}{
+		{
+			name:    "same response",
+			current: populatedResp,
+			new:     populatedResp,
+			merged:  populatedResp,
+			err:     nil,
+		},
+		{
+			name: "different upfront",
+			current: ChannelAcceptResponse{
+				UpfrontShutdown: addr1,
+			},
+			new: ChannelAcceptResponse{
+				UpfrontShutdown: addr2,
+			},
+			err: fieldMismatchError(fieldUpfrontShutdown, addr1, addr2),
+		},
+		{
+			name: "different csv",
+			current: ChannelAcceptResponse{
+				CSVDelay: 1,
+			},
+			new: ChannelAcceptResponse{
+				CSVDelay: 2,
+			},
+			err: fieldMismatchError(fieldCSV, 1, 2),
+		},
+		{
+			name: "different reserve",
+			current: ChannelAcceptResponse{
+				Reserve: 1,
+			},
+			new: ChannelAcceptResponse{
+				Reserve: 2,
+			},
+			err: fieldMismatchError(fieldReserve, 1, 2),
+		},
+		{
+			name: "different in flight",
+			current: ChannelAcceptResponse{
+				InFlightTotal: 1,
+			},
+			new: ChannelAcceptResponse{
+				InFlightTotal: 2,
+			},
+			err: fieldMismatchError(
+				fieldInFlightTotal, lnwire.MilliSatoshi(1),
+				lnwire.MilliSatoshi(2),
+			),
+		},
+		{
+			name: "different htlc limit",
+			current: ChannelAcceptResponse{
+				HtlcLimit: 1,
+			},
+			new: ChannelAcceptResponse{
+				HtlcLimit: 2,
+			},
+			err: fieldMismatchError(fieldHtlcLimit, 1, 2),
+		},
+		{
+			name: "different min in",
+			current: ChannelAcceptResponse{
+				MinHtlcIn: 1,
+			},
+			new: ChannelAcceptResponse{
+				MinHtlcIn: 2,
+			},
+			err: fieldMismatchError(
+				fieldMinIn, lnwire.MilliSatoshi(1),
+				lnwire.MilliSatoshi(2),
+			),
+		},
+		{
+			name: "different depth",
+			current: ChannelAcceptResponse{
+				MinAcceptDepth: 1,
+			},
+			new: ChannelAcceptResponse{
+				MinAcceptDepth: 2,
+			},
+			err: fieldMismatchError(fieldMinDep, 1, 2),
+		},
+		{
+			name: "merge all values",
+			current: ChannelAcceptResponse{
+				UpfrontShutdown: lnwire.DeliveryAddress{1},
+				CSVDelay:        1,
+				Reserve:         0,
+				InFlightTotal:   3,
+				HtlcLimit:       0,
+				MinHtlcIn:       5,
+				MinAcceptDepth:  0,
+			},
+			new: ChannelAcceptResponse{
+				UpfrontShutdown: nil,
+				CSVDelay:        0,
+				Reserve:         2,
+				InFlightTotal:   0,
+				HtlcLimit:       4,
+				MinHtlcIn:       0,
+				MinAcceptDepth:  6,
+			},
+			merged: ChannelAcceptResponse{
+				UpfrontShutdown: lnwire.DeliveryAddress{1},
+				CSVDelay:        1,
+				Reserve:         2,
+				InFlightTotal:   3,
+				HtlcLimit:       4,
+				MinHtlcIn:       5,
+				MinAcceptDepth:  6,
+			},
+			err: nil,
+		},
+		{
+			// Test the case where fields have the same non-zero
+			// value, and the case where only response value is
+			// non-zero.
+			name: "empty and identical",
+			current: ChannelAcceptResponse{
+				CSVDelay:      1,
+				Reserve:       2,
+				InFlightTotal: 0,
+			},
+			new: ChannelAcceptResponse{
+				CSVDelay:      0,
+				Reserve:       2,
+				InFlightTotal: 3,
+			},
+			merged: ChannelAcceptResponse{
+				CSVDelay:      1,
+				Reserve:       2,
+				InFlightTotal: 3,
+			},
+			err: nil,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+
+		t.Run(test.name, func(t *testing.T) {
+			resp, err := mergeResponse(test.current, test.new)
+			require.Equal(t, test.err, err)
+
+			// If we expect an error, exit early rather than compare
+			// our result.
+			if test.err != nil {
+				return
+			}
+
+			require.Equal(t, test.merged, resp)
+		})
+	}
+}