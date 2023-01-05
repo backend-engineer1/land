carlaKC/lnd

multi: add custom message overrides to protocol config options

lncfg/protocol_experimental_off.go
@@ -7,3 +7,9 @@ package lncfg
 // features that also require a build-tag to activate.
 type ExperimentalProtocol struct {
 }
+
+// CustomMessageOverrides returns the set of protocol messages that we override
+// to allow custom handling.
+func (p ExperimentalProtocol) CustomMessageOverrides() []uint64 {
+	return nil
+}

backend-engineer1/land

lncfg/protocol_experimental_off.go
@@ -7,3 +7,9 @@ package lncfg
 // features that also require a build-tag to activate.
 type ExperimentalProtocol struct {
 }
+
+// CustomMessageOverrides returns the set of protocol messages that we override
+// to allow custom handling.
+func (p ExperimentalProtocol) CustomMessageOverrides() []uint16 {
+	return nil
+}