ellemouton/lnd

watchtower: add GetTower to tower iterator
Add a GetTower method to the tower iterator.

watchtower/wtclient/errors.go
@@ -1,6 +1,9 @@
 package wtclient
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	// ErrClientExiting signals that the watchtower client is shutting down.
@@ -10,6 +13,10 @@ var (
 	// cycled through all available candidates.
 	ErrTowerCandidatesExhausted = errors.New("exhausted all tower " +
 		"candidates")
+
+	// ErrTowerNotInIterator is returned when a requested tower was not
+	// found in the iterator.
+	ErrTowerNotInIterator = fmt.Errorf("tower not in iterator")
 
 	// ErrPermanentTowerFailure signals that the tower has reported that it
 	// has permanently failed or the client believes this has happened based

backend-engineer1/land

watchtower/wtclient/errors.go
@@ -1,6 +1,8 @@
 package wtclient
 
-import "errors"
+import (
+	"errors"
+)
 
 var (
 	// ErrClientExiting signals that the watchtower client is shutting down.
@@ -10,6 +12,10 @@ var (
 	// cycled through all available candidates.
 	ErrTowerCandidatesExhausted = errors.New("exhausted all tower " +
 		"candidates")
+
+	// ErrTowerNotInIterator is returned when a requested tower was not
+	// found in the iterator.
+	ErrTowerNotInIterator = errors.New("tower not in iterator")
 
 	// ErrPermanentTowerFailure signals that the tower has reported that it
 	// has permanently failed or the client believes this has happened based