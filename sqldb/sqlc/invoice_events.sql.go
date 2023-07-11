lightningnetwork/lnd

sqlc: generate go code from SQL
run `make sqlc`

All the code in this commit is auto-generated.

sqldb/sqlc/invoice_events.sql.go
@@ -1,0 +1,128 @@
+// Code generated by sqlc. DO NOT EDIT.
+// versions:
+//   sqlc v1.18.0
+// source: invoice_events.sql
+
+package sqlc
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+const deleteInvoiceEvents = `-- name: DeleteInvoiceEvents :exec
+DELETE
+FROM invoice_events
+WHERE invoice_id = $1
+`
+
+func (q *Queries) DeleteInvoiceEvents(ctx context.Context, invoiceID int32) error {
+	_, err := q.db.ExecContext(ctx, deleteInvoiceEvents, invoiceID)
+	return err
+}
+
+const insertInvoiceEvent = `-- name: InsertInvoiceEvent :exec
+INSERT INTO invoice_events (
+    created_at, invoice_id, htlc_id, set_id, event_type, event_metadata
+) VALUES (
+    $1, $2, $3, $4, $5, $6
+)
+`
+
+type InsertInvoiceEventParams struct {
+	CreatedAt     time.Time
+	InvoiceID     int32
+	HtlcID        sql.NullInt32
+	SetID         sql.NullInt32
+	EventType     int32
+	EventMetadata []byte
+}
+
+func (q *Queries) InsertInvoiceEvent(ctx context.Context, arg InsertInvoiceEventParams) error {
+	_, err := q.db.ExecContext(ctx, insertInvoiceEvent,
+		arg.CreatedAt,
+		arg.InvoiceID,
+		arg.HtlcID,
+		arg.SetID,
+		arg.EventType,
+		arg.EventMetadata,
+	)
+	return err
+}
+
+const selectInvoiceEvents = `-- name: SelectInvoiceEvents :many
+SELECT id, created_at, invoice_id, htlc_id, set_id, event_type, event_metadata
+FROM invoice_events
+WHERE (
+    invoice_id = $1 OR 
+    $1 IS NULL
+) AND (
+    htlc_id = $2 OR 
+    $2 IS NULL
+) AND (
+    set_id = $3 OR 
+    $3 IS NULL
+) AND (
+    event_type = $4 OR 
+    $4 IS NULL
+) AND (
+    created_at >= $5 OR 
+    $5 IS NULL
+) AND (
+    created_at <= $6 OR 
+    $6 IS NULL
+) 
+LIMIT $8 OFFSET $7
+`
+
+type SelectInvoiceEventsParams struct {
+	InvoiceID     sql.NullInt32
+	HtlcID        sql.NullInt32
+	SetID         sql.NullInt32
+	EventType     sql.NullInt32
+	CreatedAfter  sql.NullTime
+	CreatedBefore sql.NullTime
+	NumOffset     int32
+	NumLimit      int32
+}
+
+func (q *Queries) SelectInvoiceEvents(ctx context.Context, arg SelectInvoiceEventsParams) ([]InvoiceEvent, error) {
+	rows, err := q.db.QueryContext(ctx, selectInvoiceEvents,
+		arg.InvoiceID,
+		arg.HtlcID,
+		arg.SetID,
+		arg.EventType,
+		arg.CreatedAfter,
+		arg.CreatedBefore,
+		arg.NumOffset,
+		arg.NumLimit,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []InvoiceEvent
+	for rows.Next() {
+		var i InvoiceEvent
+		if err := rows.Scan(
+			&i.ID,
+			&i.CreatedAt,
+			&i.InvoiceID,
+			&i.HtlcID,
+			&i.SetID,
+			&i.EventType,
+			&i.EventMetadata,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}

sqldb/sqlc/models.go
@@ -1,0 +1,93 @@
+// Code generated by sqlc. DO NOT EDIT.
+// versions:
+//   sqlc v1.18.0
+
+package sqlc
+
+import (
+	"database/sql"
+	"time"
+)
+
+type AmpInvoiceHtlc struct {
+	SetID      []byte
+	HtlcID     string
+	InvoiceID  int32
+	RootShare  []byte
+	ChildIndex int64
+	Hash       []byte
+	Preimage   []byte
+}
+
+type AmpInvoicePayment struct {
+	SetID        []byte
+	State        int16
+	CreatedAt    time.Time
+	SettledIndex sql.NullInt32
+	InvoiceID    int32
+}
+
+type Invoice struct {
+	ID             int32
+	Hash           []byte
+	Preimage       []byte
+	Memo           sql.NullString
+	AmountMsat     int64
+	CltvDelta      int32
+	Expiry         int32
+	PaymentAddr    []byte
+	PaymentRequest sql.NullString
+	State          int16
+	AmountPaidMsat int64
+	IsAmp          bool
+	IsHodl         bool
+	IsKeysend      bool
+	CreatedAt      time.Time
+}
+
+type InvoiceEvent struct {
+	ID            int32
+	CreatedAt     time.Time
+	InvoiceID     int32
+	HtlcID        sql.NullInt32
+	SetID         sql.NullInt32
+	EventType     int32
+	EventMetadata []byte
+}
+
+type InvoiceEventType struct {
+	ID          int32
+	Description string
+}
+
+type InvoiceFeature struct {
+	Feature   int32
+	InvoiceID int32
+}
+
+type InvoiceHtlc struct {
+	ID           int32
+	HtlcID       string
+	ChanID       string
+	AmountMsat   int64
+	TotalMppMsat sql.NullInt64
+	AcceptHeight int32
+	AcceptTime   time.Time
+	ExpiryHeight int32
+	State        int16
+	ResolveTime  sql.NullTime
+	InvoiceID    int32
+}
+
+type InvoiceHtlcCustomRecord struct {
+	Key    int64
+	Value  []byte
+	HtlcID string
+}
+
+type InvoicePayment struct {
+	ID             int32
+	SettledAt      time.Time
+	AmountPaidMsat int64
+	InvoiceID      int32
+}

sqldb/sqlc/querier.go
@@ -1,0 +1,51 @@
+// Code generated by sqlc. DO NOT EDIT.
+// versions:
+//   sqlc v1.18.0
+
+package sqlc
+
+import (
+	"context"
+)
+
+type Querier interface {
+	DeleteAMPHTLCCustomRecords(ctx context.Context, invoiceID int32) error
+	DeleteAMPHTLCs(ctx context.Context, invoiceID int32) error
+	DeleteAMPInvoiceHTLC(ctx context.Context, setID []byte) error
+	DeleteInvoice(ctx context.Context, arg DeleteInvoiceParams) error
+	DeleteInvoiceEvents(ctx context.Context, invoiceID int32) error
+	DeleteInvoiceFeatures(ctx context.Context, invoiceID int32) error
+	DeleteInvoiceHTLC(ctx context.Context, htlcID string) error
+	DeleteInvoiceHTLCCustomRecords(ctx context.Context, invoiceID int32) error
+	DeleteInvoiceHTLCs(ctx context.Context, invoiceID int32) error
+	FilterInvoicePayments(ctx context.Context, arg FilterInvoicePaymentsParams) ([]FilterInvoicePaymentsRow, error)
+	FilterInvoices(ctx context.Context, arg FilterInvoicesParams) ([]Invoice, error)
+	GetAMPInvoiceHTLCsByInvoiceID(ctx context.Context, invoiceID int32) ([]AmpInvoiceHtlc, error)
+	GetAMPInvoiceHTLCsBySetID(ctx context.Context, setID []byte) ([]AmpInvoiceHtlc, error)
+	// This method may return more than one invoice if filter using multiple fields
+	// from different invoices. It is the caller's responsibility to ensure that
+	// we bubble up an error in those cases.
+	GetInvoice(ctx context.Context, arg GetInvoiceParams) ([]Invoice, error)
+	GetInvoiceFeatures(ctx context.Context, invoiceID int32) ([]InvoiceFeature, error)
+	GetInvoiceHTLCCustomRecords(ctx context.Context, invoiceID int32) ([]GetInvoiceHTLCCustomRecordsRow, error)
+	GetInvoiceHTLCs(ctx context.Context, invoiceID int32) ([]InvoiceHtlc, error)
+	GetInvoicePayments(ctx context.Context, invoiceID int32) ([]InvoicePayment, error)
+	GetSetIDHTLCsCustomRecords(ctx context.Context, setID []byte) ([]GetSetIDHTLCsCustomRecordsRow, error)
+	InsertAMPInvoiceHTLC(ctx context.Context, arg InsertAMPInvoiceHTLCParams) error
+	InsertAMPInvoicePayment(ctx context.Context, arg InsertAMPInvoicePaymentParams) error
+	InsertInvoice(ctx context.Context, arg InsertInvoiceParams) (int32, error)
+	InsertInvoiceEvent(ctx context.Context, arg InsertInvoiceEventParams) error
+	InsertInvoiceFeature(ctx context.Context, arg InsertInvoiceFeatureParams) error
+	InsertInvoiceHTLC(ctx context.Context, arg InsertInvoiceHTLCParams) error
+	InsertInvoiceHTLCCustomRecord(ctx context.Context, arg InsertInvoiceHTLCCustomRecordParams) error
+	InsertInvoicePayment(ctx context.Context, arg InsertInvoicePaymentParams) (int32, error)
+	SelectAMPInvoicePayments(ctx context.Context, arg SelectAMPInvoicePaymentsParams) ([]SelectAMPInvoicePaymentsRow, error)
+	SelectInvoiceEvents(ctx context.Context, arg SelectInvoiceEventsParams) ([]InvoiceEvent, error)
+	UpdateAMPInvoiceHTLC(ctx context.Context, arg UpdateAMPInvoiceHTLCParams) error
+	UpdateAMPPayment(ctx context.Context, arg UpdateAMPPaymentParams) error
+	UpdateInvoice(ctx context.Context, arg UpdateInvoiceParams) error
+	UpdateInvoiceHTLC(ctx context.Context, arg UpdateInvoiceHTLCParams) error
+	UpdateInvoiceHTLCs(ctx context.Context, arg UpdateInvoiceHTLCsParams) error
+}
+
+var _ Querier = (*Queries)(nil)

backend-engineer1/land

sqldb/sqlc/invoice_events.sql.go
@@ -1,0 +1,128 @@
+// Code generated by sqlc. DO NOT EDIT.
+// versions:
+//   sqlc v1.18.0
+// source: invoice_events.sql
+
+package sqlc
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+const deleteInvoiceEvents = `-- name: DeleteInvoiceEvents :exec
+DELETE
+FROM invoice_events
+WHERE invoice_id = $1
+`
+
+func (q *Queries) DeleteInvoiceEvents(ctx context.Context, invoiceID int32) error {
+	_, err := q.db.ExecContext(ctx, deleteInvoiceEvents, invoiceID)
+	return err
+}
+
+const insertInvoiceEvent = `-- name: InsertInvoiceEvent :exec
+INSERT INTO invoice_events (
+    created_at, invoice_id, htlc_id, set_id, event_type, event_metadata
+) VALUES (
+    $1, $2, $3, $4, $5, $6
+)
+`
+
+type InsertInvoiceEventParams struct {
+	CreatedAt     time.Time
+	InvoiceID     int32
+	HtlcID        sql.NullInt64
+	SetID         []byte
+	EventType     int32
+	EventMetadata []byte
+}
+
+func (q *Queries) InsertInvoiceEvent(ctx context.Context, arg InsertInvoiceEventParams) error {
+	_, err := q.db.ExecContext(ctx, insertInvoiceEvent,
+		arg.CreatedAt,
+		arg.InvoiceID,
+		arg.HtlcID,
+		arg.SetID,
+		arg.EventType,
+		arg.EventMetadata,
+	)
+	return err
+}
+
+const selectInvoiceEvents = `-- name: SelectInvoiceEvents :many
+SELECT id, created_at, invoice_id, htlc_id, set_id, event_type, event_metadata
+FROM invoice_events
+WHERE (
+    invoice_id = $1 OR 
+    $1 IS NULL
+) AND (
+    htlc_id = $2 OR 
+    $2 IS NULL
+) AND (
+    set_id = $3 OR 
+    $3 IS NULL
+) AND (
+    event_type = $4 OR 
+    $4 IS NULL
+) AND (
+    created_at >= $5 OR 
+    $5 IS NULL
+) AND (
+    created_at <= $6 OR 
+    $6 IS NULL
+) 
+LIMIT $8 OFFSET $7
+`
+
+type SelectInvoiceEventsParams struct {
+	InvoiceID     sql.NullInt32
+	HtlcID        sql.NullInt64
+	SetID         []byte
+	EventType     sql.NullInt32
+	CreatedAfter  sql.NullTime
+	CreatedBefore sql.NullTime
+	NumOffset     int32
+	NumLimit      int32
+}
+
+func (q *Queries) SelectInvoiceEvents(ctx context.Context, arg SelectInvoiceEventsParams) ([]InvoiceEvent, error) {
+	rows, err := q.db.QueryContext(ctx, selectInvoiceEvents,
+		arg.InvoiceID,
+		arg.HtlcID,
+		arg.SetID,
+		arg.EventType,
+		arg.CreatedAfter,
+		arg.CreatedBefore,
+		arg.NumOffset,
+		arg.NumLimit,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []InvoiceEvent
+	for rows.Next() {
+		var i InvoiceEvent
+		if err := rows.Scan(
+			&i.ID,
+			&i.CreatedAt,
+			&i.InvoiceID,
+			&i.HtlcID,
+			&i.SetID,
+			&i.EventType,
+			&i.EventMetadata,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}

sqldb/sqlc/models.go
@@ -1,0 +1,93 @@
+// Code generated by sqlc. DO NOT EDIT.
+// versions:
+//   sqlc v1.18.0
+
+package sqlc
+
+import (
+	"database/sql"
+	"time"
+)
+
+type AmpInvoiceHtlc struct {
+	SetID      []byte
+	HtlcID     int64
+	InvoiceID  int32
+	RootShare  []byte
+	ChildIndex int64
+	Hash       []byte
+	Preimage   []byte
+}
+
+type AmpInvoicePayment struct {
+	SetID        []byte
+	State        int16
+	CreatedAt    time.Time
+	SettledIndex sql.NullInt32
+	InvoiceID    int32
+}
+
+type Invoice struct {
+	ID             int32
+	Hash           []byte
+	Preimage       []byte
+	Memo           sql.NullString
+	AmountMsat     int64
+	CltvDelta      sql.NullInt32
+	Expiry         int32
+	PaymentAddr    []byte
+	PaymentRequest sql.NullString
+	State          int16
+	AmountPaidMsat int64
+	IsAmp          bool
+	IsHodl         bool
+	IsKeysend      bool
+	CreatedAt      time.Time
+}
+
+type InvoiceEvent struct {
+	ID            int32
+	CreatedAt     time.Time
+	InvoiceID     int32
+	HtlcID        sql.NullInt64
+	SetID         []byte
+	EventType     int32
+	EventMetadata []byte
+}
+
+type InvoiceEventType struct {
+	ID          int32
+	Description string
+}
+
+type InvoiceFeature struct {
+	Feature   int32
+	InvoiceID int32
+}
+
+type InvoiceHtlc struct {
+	ID           int32
+	HtlcID       int64
+	ChanID       string
+	AmountMsat   int64
+	TotalMppMsat sql.NullInt64
+	AcceptHeight int32
+	AcceptTime   time.Time
+	ExpiryHeight int32
+	State        int16
+	ResolveTime  sql.NullTime
+	InvoiceID    int32
+}
+
+type InvoiceHtlcCustomRecord struct {
+	Key    int64
+	Value  []byte
+	HtlcID int64
+}
+
+type InvoicePayment struct {
+	ID             int32
+	SettledAt      time.Time
+	AmountPaidMsat int64
+	InvoiceID      int32
+}

sqldb/sqlc/querier.go
@@ -1,0 +1,51 @@
+// Code generated by sqlc. DO NOT EDIT.
+// versions:
+//   sqlc v1.18.0
+
+package sqlc
+
+import (
+	"context"
+)
+
+type Querier interface {
+	DeleteAMPHTLCCustomRecords(ctx context.Context, invoiceID int32) error
+	DeleteAMPHTLCs(ctx context.Context, invoiceID int32) error
+	DeleteAMPInvoiceHTLC(ctx context.Context, setID []byte) error
+	DeleteInvoice(ctx context.Context, arg DeleteInvoiceParams) error
+	DeleteInvoiceEvents(ctx context.Context, invoiceID int32) error
+	DeleteInvoiceFeatures(ctx context.Context, invoiceID int32) error
+	DeleteInvoiceHTLC(ctx context.Context, htlcID int64) error
+	DeleteInvoiceHTLCCustomRecords(ctx context.Context, invoiceID int32) error
+	DeleteInvoiceHTLCs(ctx context.Context, invoiceID int32) error
+	FilterInvoicePayments(ctx context.Context, arg FilterInvoicePaymentsParams) ([]FilterInvoicePaymentsRow, error)
+	FilterInvoices(ctx context.Context, arg FilterInvoicesParams) ([]Invoice, error)
+	GetAMPInvoiceHTLCsByInvoiceID(ctx context.Context, invoiceID int32) ([]AmpInvoiceHtlc, error)
+	GetAMPInvoiceHTLCsBySetID(ctx context.Context, setID []byte) ([]AmpInvoiceHtlc, error)
+	// This method may return more than one invoice if filter using multiple fields
+	// from different invoices. It is the caller's responsibility to ensure that
+	// we bubble up an error in those cases.
+	GetInvoice(ctx context.Context, arg GetInvoiceParams) ([]Invoice, error)
+	GetInvoiceFeatures(ctx context.Context, invoiceID int32) ([]InvoiceFeature, error)
+	GetInvoiceHTLCCustomRecords(ctx context.Context, invoiceID int32) ([]GetInvoiceHTLCCustomRecordsRow, error)
+	GetInvoiceHTLCs(ctx context.Context, invoiceID int32) ([]InvoiceHtlc, error)
+	GetInvoicePayments(ctx context.Context, invoiceID int32) ([]InvoicePayment, error)
+	GetSetIDHTLCsCustomRecords(ctx context.Context, setID []byte) ([]GetSetIDHTLCsCustomRecordsRow, error)
+	InsertAMPInvoiceHTLC(ctx context.Context, arg InsertAMPInvoiceHTLCParams) error
+	InsertAMPInvoicePayment(ctx context.Context, arg InsertAMPInvoicePaymentParams) error
+	InsertInvoice(ctx context.Context, arg InsertInvoiceParams) (int32, error)
+	InsertInvoiceEvent(ctx context.Context, arg InsertInvoiceEventParams) error
+	InsertInvoiceFeature(ctx context.Context, arg InsertInvoiceFeatureParams) error
+	InsertInvoiceHTLC(ctx context.Context, arg InsertInvoiceHTLCParams) error
+	InsertInvoiceHTLCCustomRecord(ctx context.Context, arg InsertInvoiceHTLCCustomRecordParams) error
+	InsertInvoicePayment(ctx context.Context, arg InsertInvoicePaymentParams) (int32, error)
+	SelectAMPInvoicePayments(ctx context.Context, arg SelectAMPInvoicePaymentsParams) ([]SelectAMPInvoicePaymentsRow, error)
+	SelectInvoiceEvents(ctx context.Context, arg SelectInvoiceEventsParams) ([]InvoiceEvent, error)
+	UpdateAMPInvoiceHTLC(ctx context.Context, arg UpdateAMPInvoiceHTLCParams) error
+	UpdateAMPPayment(ctx context.Context, arg UpdateAMPPaymentParams) error
+	UpdateInvoice(ctx context.Context, arg UpdateInvoiceParams) error
+	UpdateInvoiceHTLC(ctx context.Context, arg UpdateInvoiceHTLCParams) error
+	UpdateInvoiceHTLCs(ctx context.Context, arg UpdateInvoiceHTLCsParams) error
+}
+
+var _ Querier = (*Queries)(nil)